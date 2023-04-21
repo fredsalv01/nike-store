16f508a04d69d847c23e5f8ca291f28ab7db63c5
fredalbert37 <fredalbert37@outlook.com>
2023-04-21 00:16:02 -0500

    product screen

---
+++ src/screens/ProductsScreen.js
@@ -0,0 +1,36 @@
+import { FlatList, Image, StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+import products from "../data/products";
+
+const ProductsScreen = () => {
+  return (
+    <FlatList
+      data={products}
+      renderItem={({ item }) => (
+        <View style={styles.itemContainer}>
+          <Image
+            source={{
+              uri: item.image,
+            }}
+            style={styles.image}
+          />
+        </View>
+      )}
+      numColumns={2}
+    />
+  );
+};
+
+export default ProductsScreen;
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    width: "50%",
+    padding: 1,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+});
